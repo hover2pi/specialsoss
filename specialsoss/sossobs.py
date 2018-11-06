61b78586b44fdcaeca926a9c5a32e2b5d037519a
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-11-06 16:14:07 -0500

    Create sossobs.py

---
+++ specialsoss/sossobs.py
@@ -0,0 +1,15 @@
+"""A module to perform optimal spectral extraction of SOSS time series observations"""
+import numpy as np
+
+
+class SossObs:
+    """A class object to extract and manipulate SOSS spectra"""
+    def __init__(self, path):
+        """Initialize the SOSS extraction object
+        
+        Parameters
+        ----------
+        path: str
+            The path to the SOSS data
+        """
+        pass
