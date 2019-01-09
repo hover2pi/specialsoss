57a96cff095d84521157ab9bfed89eb3a05972f0
Joe Filippazzo <jfilippazzo@stsci.edu>
2019-01-09 11:58:02 -0500

    Added nice plotting for isolate_trace

--- specialsoss/locate_trace.py
+++ specialsoss/locate_trace.py
@@ -1,13 +1,14 @@
 """Module to locate the signal traces in SOSS 2D frames"""
 import os
-import multiprocessing
+from functools import partial
+from multiprocessing.dummy import Pool as ThreadPool
 import time
 import warnings
 
 from bokeh.plotting import figure, show
+from bokeh.models import Span
+from bokeh.models.glyphs import Step
 import numpy as np
-from functools import partial
-from numpy import ma
 from scipy.optimize import curve_fit
 
 from . import crossdispersion as xdsp
@@ -15,319 +16,54 @@ from . import crossdispersion as xdsp
 warnings.simplefilter('ignore')
 
 
-def function_traces(data2D, start1=(4,75), start2=(520,145), xdisp=20, offset=0.5, **kwargs):
+def find_orders(frame, n_jobs=4, plot=True, **kwargs):
     """
-    Return the given data with non-trace pixels masked
-    
-    Parameters
-    ==========
-    data2D: array-like
-        The 2D data to search for a trace
-    start1: tuple
-        The (row,column) to start looking for order 1
-    start2: tuple
-        The (row,column) to start looking for order 2
-    xdisp: int
-        The radius of the psf in the cross-dispersion direction
-    plot: bool
-        Plot the masked 2D data
-        
-    Returns
-    =======
-    np.ndarray
-        The given 2D data with non-signal pixels masked
-        
-    """
-    # Make a masked array of the 2D data
-    masked_data1 = ma.array(data2D.copy(), mask=False)
-    masked_data2 = ma.array(data2D.copy(), mask=False)
-    
-    # Set initial constraints
-    llim1 = start1[1]-xdisp-5
-    ulim1 = start1[1]+xdisp+5
-    llim2 = start2[1]-xdisp-5
-    ulim2 = start2[1]+xdisp+5
-        
-    # Replace each column mask with the one calculated from isolate_signal,
-    # using the mask of the previous column as curve_fit constraints
-    for n,col in enumerate(masked_data1):
-            
-        # Update constraints
-        if n > 0:
-            
-            # For order 1
-            try:
-                if llim1 < 5:
-                    llim1 = 5
-                    ulim1 = 5+xdisp*2
-                elif np.where(~masked_data1.mask[n-1])[0][0] < llim1:
-                    llim1 -= offset
-                    ulim1 -= offset
-                else:
-                    llim1 += offset
-                    ulim1 += offset
-                    
-            except:
-                pass
-                
-            # For order 2
-            try:
-                
-                if np.where(~masked_data2.mask[n-1])[0][0] < llim2:
-                    llim2 -= offset
-                    ulim2 -= offset
-                else:
-                    llim2 += offset
-                    ulim2 += offset
-                    
-            except:
-                pass
-                
-        # Only extract order 1
-        if n > start1[0] and n < start2[0]:
-            
-            try:
-                bounds = ([llim1, 1, 5, 1, 5, 3], [ulim1, 5, 2500, 10, 2500, 30])
-                col_mask1 = isolate_signal(col, func=batman, xdisp=xdisp, bounds=bounds)
-            except:
-                col_mask1 = ma.array(col, mask=masked_data1.mask[n-1])
-            
-            col_mask2 = ma.array(col, mask=True)
-            
-        # Extract both orders
-        elif n > start1[0] and n > start2[0]:
-            
-            try:
-                bounds = ([llim1, 2, 5, 2, 5, 3, llim2, 2, 3, 2, 3],
-                          [ulim1, 5, 2500, 10, 2500, 30, ulim2, 4, 10, 8, 10])
-                col_mask1, col_mask2 = isolate_orders(col, bounds=bounds, xdisp=xdisp)
-            except:
-                col_mask1 = ma.array(col, mask=masked_data1.mask[n-1])
-                col_mask2 = ma.array(col, mask=masked_data2.mask[n-1])
-                
-        # Don't extract either order
-        else:
-            col_mask1 = ma.array(col, mask=True)
-            col_mask2 = ma.array(col, mask=True)
-            
-        # Set the mask in the image
-        masked_data1.mask[n] = col_mask1.mask
-        masked_data2.mask[n] = col_mask2.mask
-
-    # Smooth the trace
-    smooth_masked_data1 = smooth_trace(masked_data1, 4, xdisp, start1[0])
-    try:
-        smooth_masked_data2 = smooth_trace(masked_data2, 3, xdisp, start2[0])
-    except:
-        smooth_masked_data2 = ''
-
-    # Plot it
-    plt.figure(figsize=(13,2))
-    plt.imshow(masked_data1.data, origin='lower', norm=LogNorm())
-    plt.imshow(smooth_masked_data1, origin='lower', norm=LogNorm(), cmap=plt.cm.Blues_r, alpha=0.7)
-    try:
-        plt.imshow(smooth_masked_data2, origin='lower', norm=LogNorm(), cmap=plt.cm.Reds_r, alpha=0.7)
-    except:
-        pass
-    plt.xlim(0, 2048)
-    plt.ylim(0, 256)
-    plt.show()
-        
-    return smooth_masked_data1, smooth_masked_data2
-
-def smooth_trace(masked_data, order, width, start, plot=True):
-    """
-    Smooth a trace by fitting a polynomial to the unmasked pixels
-    
-    Parameters
-    ----------
-    masked_data: numpy.ma.array
-        The masked data to smooth
-    order: int
-        The polynomial order to fit to the unmasked data
-    width: int
-        The desired radius of the trace from the best fit line
-    start: int
-        The index of the first pixel to fit
-    plot: bool
-        Plot the original image with the smoothed fits
-    
-    Returns
-    -------
-    numpy.ma.array
-        The smoothed masked array 
-    """
-    return masked_data
-    # Make a scatter plot where the pixels in each column are offset by a small amount
-    # x, y = [], []
-    # for n, col in enumerate(masked_data):
-    #     vals = np.where(~col.mask)
-    #     if vals:
-    #         v = list(vals[0])
-    #         y += v
-    #         x += list(np.random.normal(n, 0.01, size=len(v)))
-    #
-    # # plt.imshow(masked_data.data, origin='lower', norm=LogNorm())
-    # plt.scatter(x, y)
-    # # plt.fill_between(X, bottom, top, color='b', alpha=0.3)
-    # plt.xlim(0,2048)
-    # plt.ylim(0,256)
-    # plt.show()
-    #
-    # # Now fit a polynomial to it!
-    # height, length = masked_data.shape[-1], masked_data.shape[-2]
-    # Y = np.polyfit(x[start:], y[start:], order)
-    # X = np.arange(start, length, 1)
-    #
-    # # Get pixel values of trace and top and bottom bounds
-    # trace = np.floor(np.polyval(Y, X)).round().astype(int)
-    # bottom = trace-width
-    # top = trace+width
-    #
-    # # Plot it
-    # if plot:
-    #     plt.imshow(masked_data.data, origin='lower', norm=LogNorm())
-    #     plt.plot(X, trace, color='b')
-    #     plt.fill_between(X, bottom, top, color='b', alpha=0.3)
-    #     plt.xlim(0,length)
-    #     plt.ylim(0,height)
-    #
-    # # Create smoothed mask from top and bottom polynomials
-    # smoothed_data = ma.array(masked_data.data, mask=True)
-    # for n,(b,t) in enumerate(zip(bottom,top)):
-    #     smoothed_data.mask[n+start][b:t] = False
-    #
-    # return smoothed_data
-
-def trace_polynomial(order):
-    """The polynomial that describes the order trace
+    Generate a mask of the SOSS frame based on the column fits from isolate_signal
 
     Parameters
     ----------
-    order: int
-        The order polynomial
+    frame: array-like
+        The 2D frame of SOSS data
 
     Returns
     -------
     sequence
-        The y values of the given order across the 2048 pixels
+        A masked frame for each trace order
     """
-    coeffs = [[1.71164994e-11, -4.72119272e-08, 5.10276801e-05, -5.91535309e-02, 8.30680347e+01],
-              [2.35792131e-13, 2.42999478e-08, 1.03641247e-05, -3.63088657e-02, 9.96766537e+01]]
+    # Make 2 unmasked frames
+    order1 = np.zeros_like(frame)
+    order2 = np.zeros_like(frame)
 
-    return np.polyval(coeffs[order-1], np.arange(2048))
+    # Multiprocess spectral extraction for frames
+    print("Coffee time! This takes about 3 minutes...")
+    pool = ThreadPool(n_jobs)
+    func = partial(isolate_signal, frame=frame, **kwargs)
+    masks = pool.map(func, range(5))
+    pool.close()
+    pool.join()
+
+    # Set the mask in each column
+    for n, (ord1, ord2) in enumerate(masks):
+        order1[:, n] = ord1
+        order2[:, n] = ord2
+
+    if plot:
+
+        # Make the figure
+        fig = figure(x_range=(0, frame.shape[1]), y_range=(0, frame.shape[0]),
+                     tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image")],
+                     width=int(frame.shape[1]/2), height=int(frame.shape[0]/2))
+
+        # Plot the frame
+        fig.image(image=[frame], x=0, y=0, dw=frame.shape[1],
+                  dh=frame.shape[0], palette='Viridis256')
+
+    return order1, order2
 
 
-def isolate_orders(frame):
+def isolate_signal(idx, frame, bounds=None, sigma=3, err=None, radius=None, filt='CLEAR', plot=False):
     """
-    Return the given data with non-trace pixels masked
-    
-    Parameters
-    ==========
-    frame: array-like
-        The 2D data to search for a trace
-        
-    Returns
-    =======
-    np.ndarray
-        The given 2D data with non-signal pixels masked
-        
-    """
-    # Make a masked array of the 2D data
-    masked_data1 = ma.array(frame.copy(), mask=False)
-    masked_data2 = ma.array(frame.copy(), mask=False)
-
-    # Replace each column mask with the one calculated from isolate_signal,
-    # using the mask of the previous column as curve_fit constraints
-    for n,col in enumerate(masked_data1):
-            
-        # Update constraints
-        if n > 0:
-            
-            # For order 1
-            try:
-                if llim1 < 5:
-                    llim1 = 5
-                    ulim1 = 5+xdisp*2
-                elif np.where(~masked_data1.mask[n-1])[0][0] < llim1:
-                    llim1 -= offset
-                    ulim1 -= offset
-                else:
-                    llim1 += offset
-                    ulim1 += offset
-                    
-            except:
-                pass
-                
-            # For order 2
-            try:
-                
-                if np.where(~masked_data2.mask[n-1])[0][0] < llim2:
-                    llim2 -= offset
-                    ulim2 -= offset
-                else:
-                    llim2 += offset
-                    ulim2 += offset
-                    
-            except:
-                pass
-                
-        # Only extract order 1
-        if n > start1[0] and n < start2[0]:
-            
-            try:
-                bounds = ([llim1, 1, 5, 1, 5, 3], [ulim1, 5, 2500, 10, 2500, 30])
-                col_mask1 = isolate_signal(col, func=batman, xdisp=xdisp, bounds=bounds)
-            except:
-                col_mask1 = ma.array(col, mask=masked_data1.mask[n-1])
-            
-            col_mask2 = ma.array(col, mask=True)
-            
-        # Extract both orders
-        elif n > start1[0] and n > start2[0]:
-            
-            try:
-                bounds = ([llim1, 2, 5, 2, 5, 3, llim2, 2, 3, 2, 3],
-                          [ulim1, 5, 2500, 10, 2500, 30, ulim2, 4, 10, 8, 10])
-                col_mask1, col_mask2 = isolate_orders(col, bounds=bounds, xdisp=xdisp)
-            except:
-                col_mask1 = ma.array(col, mask=masked_data1.mask[n-1])
-                col_mask2 = ma.array(col, mask=masked_data2.mask[n-1])
-                
-        # Don't extract either order
-        else:
-            col_mask1 = ma.array(col, mask=True)
-            col_mask2 = ma.array(col, mask=True)
-            
-        # Set the mask in the image
-        masked_data1.mask[n] = col_mask1.mask
-        masked_data2.mask[n] = col_mask2.mask
-
-    # Smooth the trace
-    smooth_masked_data1 = smooth_trace(masked_data1, 4, xdisp, start1[0])
-    try:
-        smooth_masked_data2 = smooth_trace(masked_data2, 3, xdisp, start2[0])
-    except:
-        smooth_masked_data2 = ''
-
-    # Plot it
-    plt.figure(figsize=(13,2))
-    plt.imshow(masked_data1.data, origin='lower', norm=LogNorm())
-    plt.imshow(smooth_masked_data1, origin='lower', norm=LogNorm(), cmap=plt.cm.Blues_r, alpha=0.7)
-    try:
-        plt.imshow(smooth_masked_data2, origin='lower', norm=LogNorm(), cmap=plt.cm.Reds_r, alpha=0.7)
-    except:
-        pass
-    plt.xlim(0, 2048)
-    plt.ylim(0, 256)
-    plt.show()
-        
-    return smooth_masked_data1, smooth_masked_data2
-
-
-def isolate_signal(idx, frame, bounds=None, sigma=3, err=None, radius=None, filt='CLEAR', plot=True):
-    """
-    Fit a mixed gaussian function to the signal in an array of data. 
+    Fit a mixed gaussian function to the signal in a column of data. 
     Identify all pixels within n-sigma as signal.
     
     Parameters
@@ -352,7 +88,10 @@ def isolate_signal(idx, frame, bounds=None, sigma=3, err=None, radius=None, filt
     """
     # Get the column of data
     col = frame[:, idx]
-    col[120:] *= 400
+
+    # Make a mask for each order
+    ord1 = np.ones_like(col)
+    ord2 = np.ones_like(col)
 
     # Use the trace centers as the position of the center peak
     x1 = trace_polynomial(1)[idx]
@@ -361,8 +100,8 @@ def isolate_signal(idx, frame, bounds=None, sigma=3, err=None, radius=None, filt
     # Set the column at which order 2 ends
     order2end = 1900 if col.size == 256 else 1050
 
-    # Two cases where there is no order 2
-    if (idx >= order2end and filt == 'CLEAR') or filt == 'F277W':
+    # No order 2 if the trace is off the detector or the filter is F277W
+    if idx >= order2end or filt.lower() == 'f277w':
 
         # Use the batman function to find only the first order
         func = xdsp.batman
@@ -394,9 +133,12 @@ def isolate_signal(idx, frame, bounds=None, sigma=3, err=None, radius=None, filt
                   [x2+3, 4, 1e3, 8, 1e3, 10, x1+3, 4, 1e6, 8, 1e6, 10])
 
     # Fit function to signal
-    col = ma.array(col, mask=False)
-    x = ma.array(np.arange(len(col)))
+    x = np.arange(col.size)
     params, cov = curve_fit(func, x, col, bounds=bounds, sigma=err)
+
+    # Switch order 1 and 2 in list
+    if len(params) == 12:
+        params = np.concatenate([params[6:], params[:6]])
 
     # -----------------------------------------------------------------------
     # Order 1
@@ -419,10 +161,8 @@ def isolate_signal(idx, frame, bounds=None, sigma=3, err=None, radius=None, filt
         llim1 = params1[0][0]-params1[0][1]*sigma
         ulim1 = params1[-1][0]+params1[-1][1]*sigma
 
-    # Make a mask for oredr 1
-    ord1 = ma.array(col)
-    ord1.mask[(x > llim1) & (x < ulim1)] = True
-    ord1.mask = np.invert(ord1.mask)
+    # Unmask order 1
+    ord1[(x > llim1) & (x < ulim1)] = 0
 
     # -----------------------------------------------------------------------
     # Order 2
@@ -447,15 +187,12 @@ def isolate_signal(idx, frame, bounds=None, sigma=3, err=None, radius=None, filt
             llim2 = params2[0][0]-params2[0][1]*sigma
             ulim2 = params2[-1][0]+params2[-1][1]*sigma
 
-        # Make a mask for order 2
-        ord2 = ma.array(col)
-        ord2.mask[(x > llim2) & (x < ulim2)] = True
-        ord2.mask = np.invert(ord2.mask)
+        # Unmask order 2
+        ord2[(x > llim2) & (x < ulim2)] = 0
 
     # Mask the whole column for order 2
     else:
-        p4 = p5 = p6 = None
-        ord2 = ma.array(col, mask=True)
+        p4 = p5 = p6 = llim2 = ulim2 = None
 
     # -----------------------------------------------------------------------
 
@@ -463,18 +200,55 @@ def isolate_signal(idx, frame, bounds=None, sigma=3, err=None, radius=None, filt
 
         # Make the figure
         fig = figure(x_range=(0, col.size), width=1000, height=600,
-                     tooltips=[("x", "$x"), ("y", "$y"), ("value", "@line")],
+                     tooltips=[("x", "$x"), ("y", "$y")],
                      title='Column {}'.format(idx))
+        fig.xaxis.axis_label = 'Row'
+        fig.yaxis.axis_label = 'Count Rate [ADU/s]'
 
         # The data
-        fig.line(x, col, color='black', legend='Data')
+        fig.step(x, col, color='black', legend='Data', mode='center')
 
-        # The fit function and components
-        fig.line(x, func(x, *params), color='red', legend='Fit')
-        for g in [p1, p2, p3, p4, p5, p6]:
-            if g is not None:
-                fig.line(x, xdsp.gaussian(x, *g), alpha=0.5, legend='Components')
+        # Plot order 1 fit with limits
+        color1 = '#2171b5'
+        bm1 = xdsp.batman(x, *params[:6])
+        fig.line(x, bm1, legend='Order 1', color=color1, line_width=2, alpha=0.8)
+        low1 = Span(location=llim1, dimension='height', line_color=color1, line_dash='dashed')
+        high1 = Span(location=ulim1, dimension='height', line_color=color1, line_dash='dashed')
+        fig.renderers.extend([low1, high1])
+        for g in [p1, p2, p3]:
+            fig.line(x, xdsp.gaussian(x, *g), alpha=0.3, color=color1)
 
+        # Plot order 2 fit with limits
+        if llim2 is not None:
+            color2 = '#DD4968'
+            bm2 = xdsp.batman(x, *params[6:])
+            fig.line(x, bm2, legend='Order 2', color=color2, line_width=2, alpha=0.8)
+            low2 = Span(location=llim2, dimension='height', line_color=color2, line_dash='dashed')
+            high2 = Span(location=ulim2, dimension='height', line_color=color2, line_dash='dashed')
+            fig.renderers.extend([low2, high2])
+            for g in [p4, p5, p6]:
+                fig.line(x, xdsp.gaussian(x, *g), alpha=0.3, color=color2)
+
+        fig.legend.click_policy = 'hide'
         show(fig)
 
     return ord1, ord2
+
+
+def trace_polynomial(order):
+    """The polynomial that describes the order trace
+
+    Parameters
+    ----------
+    order: int
+        The order polynomial
+
+    Returns
+    -------
+    sequence
+        The y values of the given order across the 2048 pixels
+    """
+    coeffs = [[1.71164994e-11, -4.72119272e-08, 5.10276801e-05, -5.91535309e-02, 8.30680347e+01],
+              [2.35792131e-13, 2.42999478e-08, 1.03641247e-05, -3.63088657e-02, 9.96766537e+01]]
+
+    return np.polyval(coeffs[order-1], np.arange(2048))
